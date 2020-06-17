aebc7857a62bb12d11de2aa78d1366920ea79d6e
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-06-17 22:58:48 +0300

    json fetch works

--- serverAPI/Controllers/WeatherForecastController.cs
+++ serverAPI/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
 
 namespace serverAPI.Controllers
 {
@@ -24,16 +27,53 @@ namespace serverAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+public  async Task<JsonResult> Get()
+{
+    // var model = new SearchModel();
+    var url = "https://jsonplaceholder.typicode.com/users";//it should be the url of your api
+    using (var httpClient = new HttpClient())
+    {
+        using (var response = await httpClient.GetAsync(url))
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            using (var content = response.Content)
+            {  
+               //get the json result from your api
+                var result = await content.ReadAsStringAsync();
+                // var root = (JObject)JsonConvert.DeserializeObject(result);
+                // var root = (JObject)JsonConvert.DeserializeObject(result);
+                // var items = root.SelectToken("responseHeader").Children().OfType<JProperty>().ToDictionary(p => p.Name, p => p.Value);
+                // foreach(var item in items)
+                // {
+                //     if(item.Key== "response")
+                //     {
+                //         var key = item.Value.SelectToken("").OfType<JProperty>().ToDictionary(p => p.Name, p => p.Value);
+
+                //         foreach (var k in key)
+                //         { 
+                //             if(k.Key== "docs")
+                //             {
+                //                 var tests = JsonConvert.DeserializeObject<JArray>(k.Value.ToString());
+                //                 var data = k.Value.SelectToken("").Children().First();
+                //                 var test = data.SelectToken("").Children().OfType<JProperty>().ToDictionary(p => p.Name, p => p.Value);
+                //                 foreach (var t in test)
+                //                 {
+                //                     if (t.Key == "url")
+                //                     {
+                //                         model.Source = t.Value.ToString();
+                //                     }
+                //                     else if (t.Key=="title")
+                //                     {
+                //                         model.Title = t.Value.ToString();
+                //                     }
+                //                 }
+                //             }
+                //         }
+                //     }
+                // }
+                return new JsonResult(result);
+            }
         }
+    }
+}
     }
 }
--- serverAPI/Startup.cs
+++ serverAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace serverAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
