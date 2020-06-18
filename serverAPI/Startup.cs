a5fa5866b87c075b35b875cf87dfc63a6ce3e54b
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-06-18 14:24:41 +0300

    cors

--- serverAPI/Startup.cs
+++ serverAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace serverAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,8 @@ namespace serverAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
